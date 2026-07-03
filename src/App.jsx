5d7d50f70880a25195ef1bf9910bd65c405047f7
peddintisrinu <peddintisrinu0@gmail.com>
2026-07-03 23:10:11 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Education from "./components/Education";
+import Certificates from "./components/Certificates";
+import ScrollToTop from "./components/ScrollToTop";
+import CursorGlow from "./components/CursorGlow";
+
+function App() {
+  return (
+   <>
+    <CursorGlow />
+
+    <Navbar />
+
+    <Hero />
+
+    <About />
+
+    <Skills />
+
+    <Education />
+
+    <Experience />
+
+    <Certificates />
+
+    <Projects />
+
+    <Contact />
+
+    <Footer />
+
+    <ScrollToTop />
+    </>
+
+
+
+  );
+}
+
+export default App;
